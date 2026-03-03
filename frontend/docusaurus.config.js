670080d8dcf100e484b79ae7dd777b03e46f354a
Shahab155 <bhuttoshahab1544@gmail.com>
2026-03-03 08:09:18 +0500

    udpate

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physical-ai-humanoid-textbook-seven.vercel.app/',
+  url: 'https://physical-ai-humanoid-textbook-five.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
